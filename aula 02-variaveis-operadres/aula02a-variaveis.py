be6a90cf2969e25739cad83301de67c08045b011
lucasfurquimlima@gmail.com <labsfiap@fiap.com.br>
2026-03-17 11:41:18 -0300

    aula 02

---
+++ aula 02-variaveis-operadres/aula02a-variaveis.py
@@ -0,0 +1,37 @@
+print("ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") #CONCATENAÇÃO DE STRINGS
+
+# comentarios de 1 linha
+'''
+Comentários de multiplas linhas 
+
+'''
+
+#variaveis
+nome = "lucas"
+idade = 18
+peso = 70
+
+print(nome, idade, peso)
+print(f"ola, {nome}!")
+
+# INPUT -- SIMULA FORMS NO CMD
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade: "))
+peso = float(input("digite seu peso: "))
+
+print(nome, idade, peso)
+
+#DESAFIO
+
+nome = input("digite seu nome")
+print("Bem Vindo!")
+
+Dia = input("digite o dia do seu nascimento")
+Mes = input("digite o mes que voce nasceu")
+Ano = input("digite o ano que voce nasceu")
+
+print(Dia, Mes, Ano)
